29827828af44c3267639e0ef53df1fec251c7827
haeram27 <haeram27@gmail.com>
2024-02-13 16:03:47 +0900

    add h2 db conf

---
+++ spring-web/src/main/java/com/example/springwebex/entity/Member.java
@@ -0,0 +1,26 @@
+package com.example.springwebex.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "member")
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+}

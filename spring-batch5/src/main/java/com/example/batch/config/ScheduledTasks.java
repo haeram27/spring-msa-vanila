23b35888a52094531d8c324d5aade2794f382a42
haeram27 <haeram27@gmail.com>
2024-04-11 10:48:46 +0900

    update flow step job

--- spring-batch5/src/main/java/com/example/batch/config/ScheduledTasks.java
+++ spring-batch5/src/main/java/com/example/batch/config/ScheduledTasks.java
@@ -14,18 +14,22 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class ScheduledTasks {
-    private AtomicBoolean enabled = new AtomicBoolean(true);
+    private AtomicBoolean singleStepBatchJobLauncherEnabled = new AtomicBoolean(false);
+    private AtomicBoolean multiStepBatchJobLauncherEnabled = new AtomicBoolean(false);
+    private AtomicBoolean flowStepBatchJobLauncherEnabled = new AtomicBoolean(true);
     private AtomicInteger batchRunCounter = new AtomicInteger(0);
 
-    private final Job multiStepJob;
+
     private final Job singleStepJob;
+    private final Job multiStepJob;
+    private final Job flowStepJob;
     private final JobLauncher jobLauncher;
 
     @Scheduled(fixedRate = 2000)
     public void singleStepBatchJobLauncher() throws Exception {
         Date date = new Date();
         System.out.println("scheduler starts at " + date);
-        if (enabled.get()) {
+        if (singleStepBatchJobLauncherEnabled.get()) {
             JobExecution jobExecution = jobLauncher.run(singleStepJob,
                     new JobParametersBuilder().addDate("launchDate", date).toJobParameters());
             batchRunCounter.incrementAndGet();
@@ -38,8 +42,21 @@ public class ScheduledTasks {
     public void multiStepBatchJobLauncher() throws Exception {
         Date date = new Date();
         System.out.println("scheduler starts at " + date);
-        if (enabled.get()) {
+        if (multiStepBatchJobLauncherEnabled.get()) {
             JobExecution jobExecution = jobLauncher.run(multiStepJob,
+                    new JobParametersBuilder().addDate("launchDate", date).toJobParameters());
+            batchRunCounter.incrementAndGet();
+            System.out.println("Batch job ends with status as " + jobExecution.getStatus());
+        }
+        System.out.println("scheduler ends ");
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public void flowStepBatchJobLauncher() throws Exception {
+        Date date = new Date();
+        System.out.println("scheduler starts at " + date);
+        if (flowStepBatchJobLauncherEnabled.get()) {
+            JobExecution jobExecution = jobLauncher.run(flowStepJob,
                     new JobParametersBuilder().addDate("launchDate", date).toJobParameters());
             batchRunCounter.incrementAndGet();
             System.out.println("Batch job ends with status as " + jobExecution.getStatus());

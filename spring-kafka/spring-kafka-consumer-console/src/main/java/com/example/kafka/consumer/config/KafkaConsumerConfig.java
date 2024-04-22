42dbe359a283fab5e01e4ce944fec6d0a309d76f
haeram27 <haeram27@gmail.com>
2024-04-22 09:58:04 +0900

    update kafka consumer configs

--- spring-kafka/spring-kafka-consumer-console/src/main/java/com/example/kafka/consumer/config/KafkaConsumerConfig.java
+++ spring-kafka/spring-kafka-consumer-console/src/main/java/com/example/kafka/consumer/config/KafkaConsumerConfig.java
@@ -24,12 +24,19 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         log.info(String.valueOf(System.nanoTime()));
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        // configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaConsumerFactory<>(configProps);
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        // configs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        // configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        // configs.put(ProducerConfig.BATCH_SIZE_CONFIG, 32768);
+        // configs.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        // configs.put(ProducerConfig.ACKS_CONFIG, "all");
+        // configs.put(ProducerConfig.RETRIES_CONFIG, 0);
+        // configs.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, 20 * 1000);
+        // configs.put(ProducerConfig.RECONNECT_BACKOFF_MAX_MS_CONFIG, 60 * 1000);
+        return new DefaultKafkaConsumerFactory<>(configs);
     }
 
     @Bean
@@ -38,6 +45,8 @@ public class KafkaConsumerConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConcurrency(3);
         factory.setConsumerFactory(consumerFactory());
+        // factory.setBatchListener(true);
+        // factory.setAutoStartup(true);
         return factory;
     }
 

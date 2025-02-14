85e6cf70d647190f1da6353bc98be495983e9b34
haeram27 <haeram27@gmail.com>
2025-02-14 12:41:14 +0900

    2025-02-14 12:41:14+09:00

---
+++ spring-empty/src/main/java/com/example/empty/SpringLifecycleTestApplication.java
@@ -0,0 +1,67 @@
+package com.example.empty;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@SpringBootApplication
+@Slf4j
+public class SpringLifecycleTestApplication {
+
+    @EventListener
+    public void onMainApplicationEvent(ApplicationEvent event) {
+        // @EventListener registered after this class bean is created so init application events can NOT be received
+        log.debug(": onMainApplicationEvent: " + event.getClass().getSimpleName());
+        /*
+            if (event instanceof ApplicationStartedEvent) {
+                log.debug("Application Started");
+            } else if (event instanceof ApplicationReadyEvent) {
+                log.debug("Application Ready");
+            } else if (event instanceof ApplicationFailedEvent) {
+                log.debug("Application Failed");
+            } else if (event instanceof ContextRefreshedEvent) {
+                log.debug("Context Refreshed");
+            } else if (event instanceof ContextClosedEvent) {
+                log.debug("Context Refreshed");
+                // implementations under HERE before application closed
+            }
+        */
+    }
+
+    public static void main(String[] args) throws Exception {
+        log.debug("*** main() ***");
+        new SpringApplicationBuilder(SpringLifecycleTestApplication.class)
+        .listeners(new ApplicationListener<ApplicationEvent>() {
+            @Override
+            public void onApplicationEvent(@SuppressWarnings("null") ApplicationEvent event) {
+                // to get Initializing Events before creation of beans(@Component)
+                log.debug("InitializingApplicationEvent: " + event.getClass().getSimpleName());
+                /*
+                    if (event instanceof ApplicationContextInitializedEvent) {
+                        log.debug("Context Initialized");
+                    } else if (event instanceof ApplicationEnvironmentPreparedEvent) {
+                        log.debug("Environment Prepared");
+                    } else if (event instanceof ApplicationPreparedEvent) {
+                        log.debug("Application Prepared");
+                    } else if (event instanceof ApplicationStartedEvent) {
+                        log.debug("Application Started");
+                    } else if (event instanceof ApplicationReadyEvent) {
+                        log.debug("Application Ready");
+                    } else if (event instanceof ApplicationFailedEvent) {
+                        log.debug("Application Failed");
+                    } else if (event instanceof ContextRefreshedEvent) {
+                        log.debug("Context Refreshed");
+                    } else if (event instanceof ContextClosedEvent) {
+                        log.debug("Context Refreshed");
+                        // implementations under HERE before application closed
+                    }
+                */
+            }
+        }).run(args);
+    }
+}

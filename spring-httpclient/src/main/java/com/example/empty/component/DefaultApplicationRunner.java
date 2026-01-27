5f34c987b6e0cc502f39ff46b4a25a4aa0390345
haeram27 <haeram27@gmail.com>
2026-01-27 17:28:18 +0900

    2026-01-27 17:28:18+09:00

---
+++ spring-httpclient/src/main/java/com/example/empty/component/DefaultApplicationRunner.java
@@ -0,0 +1,31 @@
+package com.example.empty.component;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+// @Order(2)
+public class DefaultApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("## ApplicationRunner");
+        var optNames = args.getOptionNames();
+        for (var name : optNames) {
+            log.info("option: name={}, value={}", name, args.getOptionValues(name));
+        }
+
+        var nonOptArgs = args.getNonOptionArgs();
+        for (var arg : nonOptArgs) {
+            log.info("non option args: {}", arg);
+        }
+
+        var srcArgs = args.getSourceArgs();
+        for (var arg : srcArgs) {
+            log.info("source arg: {}", arg);
+        }
+    }
+}

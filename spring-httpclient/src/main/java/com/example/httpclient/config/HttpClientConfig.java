4710ca4f71a8421812856f957d99cb83d73aeca3
haeram27 <haeram27@gmail.com>
2026-01-28 16:39:26 +0900

    add webclient logic to make async http request

--- spring-httpclient/src/main/java/com/example/httpclient/config/HttpClientConfig.java
+++ spring-httpclient/src/main/java/com/example/httpclient/config/HttpClientConfig.java
@@ -3,19 +3,26 @@ package com.example.httpclient.config;
 import java.security.GeneralSecurityException;
 import java.time.Duration;
 
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.TrustAllStrategy;
 import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.reactive.HttpComponentsClientHttpConnector;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -42,6 +49,10 @@ public class HttpClientConfig {
             .build();
     }
 
+    /*
+     * RestTemplate - Sync, Blocking HttpClient
+     */
+
     @Bean
     @Primary
     public RestTemplate trustAllRestTemplate() {
@@ -55,9 +66,16 @@ public class HttpClientConfig {
                 .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
                 .buildClassic();
 
+            // TCP(Socket) level Connection Config
+            var cc = ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.of(Duration.ofSeconds(2)))  // timeout while waiting for TCP connection establishment
+                .setSocketTimeout(Timeout.of(Duration.ofSeconds(5)))   // inactivity timeout of inter-packet
+                .build();
+
             // use connection pool
             var cm = PoolingHttpClientConnectionManagerBuilder.create()
                 .setTlsSocketStrategy(tls)
+                .setDefaultConnectionConfig(cc)
                 .build();
 
             // use BasicHttpClientConnectionManager for one-time connection
@@ -67,17 +85,27 @@ public class HttpClientConfig {
                     .build();
             */
 
+            // Application(HTTP) level Connection Config
+            var rc = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(1)) // timeout while waiting for lent a connection from connection pool
+                .setResponseTimeout(Timeout.ofSeconds(5))          // timeout while waiting for server response
+                .build();
+
             var httpClient = HttpClients.custom()
                 .setConnectionManager(cm)
+                .setDefaultRequestConfig(rc)
                 .evictExpiredConnections() // Find, close and remove connections that are stored in the connection pool that are past the server-set validity (TTL)
                 .evictIdleConnections(TimeValue.ofSeconds(10)) // evict(remove) connection has no sending request for more than 10 sec
                 .build();
 
-            // set timeout setting per request in RestTemplate (Spring driven)
+            /* 
+             * set timeout setting per request in RestTemplate (Spring driven),
+             * if timeouts set here then timeout config is not require at ConnectionConfig and RequestConfig
+             */
             var rf = new HttpComponentsClientHttpRequestFactory(httpClient);
             rf.setConnectionRequestTimeout(Duration.ofSeconds(1)); // time to resolve idle http connection from conn pool
-            rf.setConnectTimeout(Duration.ofSeconds(2)); // time to establish tcp connection
-            rf.setReadTimeout(Duration.ofSeconds(5)); // time to wait response against http request from http server
+            rf.setConnectTimeout(Duration.ofSeconds(2));           // time to establish tcp connection
+            rf.setReadTimeout(Duration.ofSeconds(5));              // time to wait response against http request from http server
 
             var template = new RestTemplate(rf);
 
@@ -93,6 +121,10 @@ public class HttpClientConfig {
         }
     }
 
+    /*
+     * RestClient - Sync, Blocking HttpClient
+     */
+
     @Bean
     @Primary
     public RestClient trustAllRestClient() {
@@ -106,9 +138,16 @@ public class HttpClientConfig {
                 .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
                 .buildClassic();
 
+            // TCP(Socket) level Connection Config
+            var cc = ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.of(Duration.ofSeconds(2)))  // timeout while waiting for TCP connection establishment
+                .setSocketTimeout(Timeout.of(Duration.ofSeconds(5)))   // inactivity timeout of inter-packet
+                .build();
+
             // use connection pool
             var cm = PoolingHttpClientConnectionManagerBuilder.create()
                 .setTlsSocketStrategy(tls)
+                .setDefaultConnectionConfig(cc)
                 .build();
 
             // use BasicHttpClientConnectionManager for one-time connection
@@ -118,17 +157,27 @@ public class HttpClientConfig {
                     .build();
             */
 
+            // Application(HTTP) level Connection Config
+            var rc = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(1)) // timeout while waiting for lent a connection from connection pool
+                .setResponseTimeout(Timeout.ofSeconds(5))          // timeout while waiting for server response
+                .build();
+
             var httpClient = HttpClients.custom()
                 .setConnectionManager(cm)
+                .setDefaultRequestConfig(rc)
                 .evictExpiredConnections() // Find, close and remove connections that are stored in the connection pool that are past the server-set validity (TTL)
                 .evictIdleConnections(TimeValue.ofSeconds(10)) // evict(remove) connection has no sending request for more than 10 sec
                 .build();
 
-            // set timeout setting per request in RestTemplate (Spring driven)
+            /* 
+             * support timeout setting per request for SYNC way HttpClient (Spring driven),
+             * if timeouts set here then timeout config is not require at ConnectionConfig and RequestConfig
+             */
             var rf = new HttpComponentsClientHttpRequestFactory(httpClient);
             rf.setConnectionRequestTimeout(Duration.ofSeconds(1)); // time to resolve idle http connection from conn pool
-            rf.setConnectTimeout(Duration.ofSeconds(2)); // time to establish tcp connection
-            rf.setReadTimeout(Duration.ofSeconds(5)); // time to wait response against http request from http server
+            rf.setConnectTimeout(Duration.ofSeconds(2));           // time to establish tcp connection
+            rf.setReadTimeout(Duration.ofSeconds(5));              // time to wait response against http request from http server
 
             return RestClient.builder()
                 .requestFactory(rf)
@@ -136,6 +185,64 @@ public class HttpClientConfig {
         } catch (GeneralSecurityException e) {
             throw new org.springframework.beans.factory.BeanCreationException(
                 "trustAllRestClient", "Failed to build SSL trust-all RestClient", e);
+        }
+    }
+
+    /*
+     * WebClient - Async, Non-Blocking HttpClient
+     */
+
+    @Bean
+    @Primary
+    public WebClient trustAllWebClient() {
+        try {
+            var sslContext = SSLContexts.custom()
+            .loadTrustMaterial(null, TrustAllStrategy.INSTANCE)
+            .build();
+
+            // TlsStrategy: async client method
+            var tls = ClientTlsStrategyBuilder.create()
+                .setSslContext(sslContext)
+                .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .buildAsync();
+
+            // TCP(Socket) level Connection Config
+            var cc = ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.of(Duration.ofSeconds(2)))  // timeout while waiting for TCP connection establishment
+                .setSocketTimeout(Timeout.of(Duration.ofSeconds(5)))   // inactivity timeout of inter-packet
+                .build();
+
+            // connection pool: async client method
+            var cm = PoolingAsyncClientConnectionManagerBuilder.create()
+                .setTlsStrategy(tls)
+                .setDefaultConnectionConfig(cc)
+                // modify to enhance performance, mainly Connection Per Route
+                .setMaxConnTotal(25)    // default: 25
+                .setMaxConnPerRoute(5)  // default: 5
+                .build();
+
+            // Application(HTTP) level Connection Config
+            var rc = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(1)) // timeout while waiting for lent a connection from connection pool
+                .setResponseTimeout(Timeout.ofSeconds(5))          // timeout while waiting for server response
+                .build();
+
+            var asyncClient = HttpAsyncClients.custom()
+                .setConnectionManager(cm)
+                .setDefaultRequestConfig(rc)
+                .evictExpiredConnections() // Find, close and remove connections that are stored in the connection pool that are past the server-set validity (TTL)
+                .evictIdleConnections(TimeValue.ofSeconds(10)) // evict(remove) connection has no sending request for more than 10 sec
+                .build();
+
+            // assign WebClient to HttpConnector (make Spring control connection lifecycle)
+            var connector = new HttpComponentsClientHttpConnector(asyncClient);
+
+        return WebClient.builder()
+            .clientConnector(connector)
+            .build();
+        } catch (GeneralSecurityException e) {
+            throw new org.springframework.beans.factory.BeanCreationException(
+                "trustAllWebClient", "Failed to build SSL trust-all WebClient", e);
         }
     }
 }
--- spring-httpclient/src/test/java/com/example/httpclient/RestClientSyncTests.java
+++ spring-httpclient/src/test/java/com/example/httpclient/RestClientSyncTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,11 +20,15 @@ public class RestClientSyncTests {
     @Qualifier("trustAllRestClient")
     private RestClient restClient;
 
+    @Autowired
+    @Qualifier("trustAllWebClient")
+    private WebClient webClient;
+
     private final String serverUrl1 = "https://jsonplaceholder.typicode.com/posts";
     private final String serverUrl2 = "https://httpbin.org/"; 
 
     @Test
-    public void run() {
+    public void sync() {
 
         var future = restClient.post()
         .uri(serverUrl1)
@@ -36,5 +41,31 @@ public class RestClientSyncTests {
         .body(String.class);
 
         System.out.println(future);
-    } 
+    }
+
+    @Test
+    public void async() {
+
+        var mono = webClient.post()
+        .uri(serverUrl1)
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .retrieve()
+        .onStatus(HttpStatusCode::is2xxSuccessful, (response) -> {
+            log.info(String.format("%s, %s", response.statusCode(), response.headers()));
+            return reactor.core.publisher.Mono.empty();
+        }).bodyToMono(String.class);
+
+        // try {
+        //     mono.toFuture().getNow("heelo");
+        // } catch (CancellationException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // } catch (CompletionException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // } catch (Exception e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+    }
 }

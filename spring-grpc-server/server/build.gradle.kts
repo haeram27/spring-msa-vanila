bfb20464ae4f9d6c0bd15bec5c6ab097dd1582e5
haeram27 <haeram27@gmail.com>
2026-06-23 09:16:49 +0900

    2026-06-23 09:16:49+09:00

--- spring-grpc-server/server/build.gradle.kts
+++ spring-grpc-server/server/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 dependencies {
     implementation(project(":api"))
+    // implementation("com.example.springgrpc:spring-grpc-server-api:1.0.$buildNumber")
     implementation(project(":client"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(platform(libs.spring.grpc.bom))
